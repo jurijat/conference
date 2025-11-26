436601e7701460b66e3ef2ff34078e5e4c965f98
Juri Jatschmenow <juri.jatschmenow@raiok.com>
2025-11-26 10:12:34 +0100

    remove border in modal on mobile

--- src/components/AgendaModal.js
+++ src/components/AgendaModal.js
@@ -189,7 +189,12 @@ export class AgendaModal {
               </div>
               ${
                 speaker.job || speaker.company
-                  ? `<div class="text-sm text-primary-gray uppercase tracking-widest leading-5">${[speaker.job, speaker.company].filter(Boolean).join(", ")}</div>`
+                  ? `<div class="text-sm text-primary-gray uppercase tracking-widest leading-5">${[
+                      speaker.job,
+                      speaker.company,
+                    ]
+                      .filter(Boolean)
+                      .join(", ")}</div>`
                   : ""
               }
             </div>
@@ -216,7 +221,12 @@ export class AgendaModal {
           </div>
           ${
             speaker.job || speaker.company
-              ? `<div class="text-sm text-primary-gray uppercase tracking-widest mb-4">${[speaker.job, speaker.company].filter(Boolean).join(", ")}</div>`
+              ? `<div class="text-sm text-primary-gray uppercase tracking-widest mb-4">${[
+                  speaker.job,
+                  speaker.company,
+                ]
+                  .filter(Boolean)
+                  .join(", ")}</div>`
               : ""
           }
           <div class="flex gap-2">
@@ -267,7 +277,7 @@ export class AgendaModal {
       >
         <!-- Modal -->
         <div
-          class="bg-bg-dark w-full h-full tablet:h-[min(820px,85vh)] tablet:max-w-[960px] flex flex-col overflow-hidden border border-border-primary relative"
+          class="bg-bg-dark w-full h-full tablet:h-[min(820px,85vh)] tablet:max-w-[960px] flex flex-col overflow-hidden tablet:border tablet:border-border-primary relative"
         >
           <button
             data-close-btn
